b02f6fcc84d17fa97a1cb4895c1fc5ca2ad0f301
Yusen Jiang <yusenjia@buffalo.edu>
2022-07-29 09:54:10 -0700

    op code

--- script.js
+++ script.js
@@ -11,13 +11,17 @@ fetch("https://jsonplaceholder.typicode.com/posts")
 
     console.log(data)
 
-    const blogs = data.map((item,index) =>{
 
-        if(index % 2) {
+
+    const blogs = data.map((item,index) =>{
+        const tag = index % 2 ===0 ? "World" : "Design"
+        const color = index % 2 ===0 ? "blue" : "green"
+
+ 
             return `          
             <article class="blogs">
                 <div>
-                    <h5 class="blog-subject blue">Design</h5>
+                    <h5 class="blog-subject ${color}">${tag}</h5>
                     <h1 class="blog-title">${item.title}</h1>
                     <h5 class="blog-data">${item.id}</h5>
                     <p class="blog-info">${item.body}</p>
@@ -25,21 +29,7 @@ fetch("https://jsonplaceholder.typicode.com/posts")
                 </div>
             </article>`
         }
-
-        else{
-            return `            
-            <article class="blogs">
-                <div>
-                    <h5 class="blog-subject blue">World</h5>
-                    <h1 class="blog-title">${item.title}</h1>
-                    <h5 class="blog-data">${item.id}</h5>
-                    <p class="blog-info">${item.body}</p>
-                    <a href="" class="blog--link-readMore">Continue reading</a>
-                </div>
-            </article>`
-            
-        }
-    })
+    )
     document.querySelector('.blog-container').innerHTML = blogs.join('')
   })
 
